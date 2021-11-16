607a83479d66f73b16f212fb16bd6435e63ecdd9
Saggaf <saggaf@nusantarabetastudio.com>
2021-11-16 17:24:58 +0700

    [FIXED] Skip database initialization
    Took 7 minutes

--- internal/pds-svc/repository/datasource_map.go
+++ internal/pds-svc/repository/datasource_map.go
@@ -3,14 +3,23 @@ package repository
 import (
 	"repo.pegadaian.co.id/ms-pds/srv-notification/internal/pds-svc/contract"
 	"repo.pegadaian.co.id/ms-pds/srv-notification/internal/pkg/nucleo/ncore"
+	"repo.pegadaian.co.id/ms-pds/srv-notification/internal/pkg/nucleo/nlogger"
 	"repo.pegadaian.co.id/ms-pds/srv-notification/internal/pkg/nucleo/nsql"
 )
+
+var log = nlogger.Get()
 
 type DataSourceMap struct {
 	Postgres *nsql.DB
 }
 
 func (a *DataSourceMap) Init(config contract.DataSourcesConfig) error {
+	// Skip if not initialized
+	if a.Postgres == nil {
+		log.Debug("Skipping db init")
+		return nil
+	}
+
 	err := a.Postgres.Init(config.Postgres)
 	if err != nil {
 		return ncore.TraceError(err)
@@ -21,6 +30,7 @@ func (a *DataSourceMap) Init(config contract.DataSourcesConfig) error {
 
 func NewDataSourceMap() DataSourceMap {
 	return DataSourceMap{
-		Postgres: new(nsql.DB),
+		// TODO: Replace init db by init => new(nsql.DB),
+		Postgres: nil,
 	}
 }
