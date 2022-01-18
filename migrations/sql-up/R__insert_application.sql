3d9797c15d69c04810d87a9971664ef93236f249
Saggaf <saggaf@nusantarabetastudio.com>
2022-01-18 11:39:20 +0700

    Fix sql insert format
    Took 26 minutes

--- migrations/sql-up/R__insert_application.sql
+++ migrations/sql-up/R__insert_application.sql
@@ -1,14 +1,4 @@
 INSERT INTO public."Application"(id, "createdAt", "updatedAt", "modifiedBy", version, metadata, name, xid)
-VALUES (1, '2020-01-01 00:00:00', '2020-01-01 00:00:00', '{
-  "id": "0",
-  "role": "SEEDER",
-  "fullName": "Seeder"
-}', 1, '{}',
-        'PDS', 'APKPDS'),
-       (2, '2020-01-01 00:00:00', '2020-01-01 00:00:00', '{
-         "id": "0",
-         "role": "SEEDER",
-         "fullName": "Seeder"
-       }', 1, '{}',
-        'PSDS', 'APKPSDS')
+VALUES (1, '2020-01-01 00:00:00', '2020-01-01 00:00:00', '{"id": "0","role": "SEEDER","fullName": "Seeder"}', 1, '{}', 'PDS', 'APKPDS'),
+       (2, '2020-01-01 00:00:00', '2020-01-01 00:00:00', '{"id": "0","role": "SEEDER","fullName": "Seeder"}', 1, '{}', 'PSDS', 'APKPSDS')
 ON CONFLICT DO NOTHING;
