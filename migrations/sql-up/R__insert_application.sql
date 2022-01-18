91c70cb41fd0cc49ac8045bea3c0e86632236089
Saggaf <saggaf@nusantarabetastudio.com>
2022-01-18 12:06:11 +0700

    Remove seeders script
    Took 13 minutes

--- migrations/sql-up/R__insert_application.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO public."Application"(id, "createdAt", "updatedAt", "modifiedBy", version, metadata, name, xid)
-VALUES (1, '2020-01-01 00:00:00', '2020-01-01 00:00:00', '{"id": "0","role": "SEEDER","fullName": "Seeder"}', 1, '{}', 'PDS', 'APKPDS'),
-       (2, '2020-01-01 00:00:00', '2020-01-01 00:00:00', '{"id": "0","role": "SEEDER","fullName": "Seeder"}', 1, '{}', 'PSDS', 'APKPSDS')
-ON CONFLICT DO NOTHING;
