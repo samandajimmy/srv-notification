595d6a0343516e600062754fefba428aba8b5a7a
Dinan R Firdaus <dinan@nusantarabetastudio.com>
2022-01-23 01:28:21 +0700

    Fix column notification table

--- migrations/sql-up/V0.2__create_notification_table.sql
+++ migrations/sql-up/V0.2__create_notification_table.sql
@@ -1,19 +1,19 @@
 -- Create Tables
 CREATE TABLE "public"."Notification"
 (
-    "id"            uuid                        NOT NULL,
-    "createdAt"     timestamp without time zone NOT NULL,
-    "updatedAt"     timestamp without time zone NULL,
-    "modifiedBy"    json                        NOT NULL,
-    "metadata"      json                        NULL,
-    "version"       bigint                      NOT NULL DEFAULT 1,
-    "userRefId"     bigint,
-    "title"         varchar(255)                NOT NULL,
-    "tagline"       varchar(255),
-    "content_short" varchar(255)                NULL,
-    "content"       TEXT                        NULL,
-    "isRead"        bool                                 DEFAULT FALSE,
-    "readAt"        timestamp without time zone NULL,
+    "id"             uuid                        NOT NULL,
+    "createdAt"      timestamp without time zone NOT NULL,
+    "updatedAt"      timestamp without time zone NULL,
+    "modifiedBy"     json                        NOT NULL,
+    "metadata"       json                        NULL,
+    "version"        bigint                      NOT NULL DEFAULT 1,
+    "userRefId"      bigint,
+    "title"          varchar(255)                NOT NULL,
+    "content"        text                        NULL,
+    "contentShort"   varchar(255)                NULL,
+    "contentEncoded" text                        NULL,
+    "isRead"         bool                        NULL     DEFAULT FALSE,
+    "readAt"         timestamp without time zone NULL,
     PRIMARY KEY ("id")
 );
 
